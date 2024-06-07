4944d2d34ea4adbd60619f4b30e5964c81ccde12
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-06-07 11:32:52 +0100

    feat: Refactor mock data and client order components

--- frontend/app/mockData.tsx
+++ frontend/app/mockData.tsx
@@ -144,3 +144,553 @@ export const mockOrders = [
     { name: 'Fries', category: 'Appetizer', quantity: 1, price: 2.99 },
     { name: 'Coke rontiec dhurhum road', category: 'Beverage', quantity: 3, price: 1.50 },
 ]
+
+
+const mockDrinks = [
+    {
+        "name": "Roos Estate Sauvignon Blanc South Africa",
+        "prices": [
+            {
+                "type": "glass",
+                "price": "5.95"
+            },
+            {
+                "type": "bottle",
+                "price": "16.95"
+            }
+        ],
+        "category": "White Wines",
+        "main_category": "Wines"
+    },
+    {
+        "name": "Jacana Chardonnay Australia",
+        "prices": [
+            {
+                "type": "bottle",
+                "price": "18.95"
+            }
+        ],
+        "category": "White Wines",
+        "main_category": "Wines"
+    },
+    {
+        "name": "Chablis, Domaine du Colombier",
+        "prices": [
+            {
+                "type": "bottle",
+                "price": "24.95"
+            }
+        ],
+        "category": "White Wines",
+        "main_category": "Wines"
+    },
+    {
+        "name": "Pacific Heights Zinfandel Blush California",
+        "prices": [
+            {
+                "type": "glass",
+                "price": "5.95"
+            },
+            {
+                "type": "bottle",
+                "price": "16.95"
+            }
+        ],
+        "category": "Rose Wines",
+        "main_category": "Wines"
+    },
+    {
+        "name": "Sapuri Pinot Grigio Rose Italy",
+        "prices": [
+            {
+                "type": "bottle",
+                "price": "18.95"
+            }
+        ],
+        "category": "Rose Wines",
+        "main_category": "Wines"
+    },
+    {
+        "name": "Roos Estate Merlot South Africa",
+        "prices": [
+            {
+                "type": "glass",
+                "price": "5.95"
+            },
+            {
+                "type": "bottle",
+                "price": "16.95"
+            }
+        ],
+        "category": "Red Wines",
+        "main_category": "Wines"
+    },
+    {
+        "name": "Elements Atorum Malbec",
+        "prices": [
+            {
+                "type": "bottle",
+                "price": "21.95"
+            }
+        ],
+        "category": "Red Wines",
+        "main_category": "Wines"
+    },
+    {
+        "name": "Navardia Rioja Bodegas Bagordi Spain",
+        "prices": [
+            {
+                "type": "bottle",
+                "price": "22.95"
+            }
+        ],
+        "category": "Red Wines",
+        "main_category": "Wines"
+    },
+    {
+        "name": "Prosecco Di Maria Italy",
+        "prices": [
+            {
+                "type": "bottle",
+                "price": "20.95"
+            },
+            {
+                "type": "20cl bottle",
+                "price": "6.25"
+            }
+        ],
+        "category": "Prosecco & Champagne",
+        "main_category": "Prosecco & Champagne"
+    },
+    {
+        "name": "Freixenet Prosecco Italy",
+        "prices": [
+            {
+                "type": "bottle",
+                "price": "22.95"
+            }
+        ],
+        "category": "Prosecco & Champagne",
+        "main_category": "Prosecco & Champagne"
+    },
+    {
+        "name": "Moet & Chandon Brut NV",
+        "prices": [
+            {
+                "type": "bottle",
+                "price": "49.95"
+            }
+        ],
+        "category": "Prosecco & Champagne",
+        "main_category": "Prosecco & Champagne"
+    },
+    {
+        "name": "Bollinger Special Cuvee NV",
+        "prices": [
+            {
+                "type": "bottle",
+                "price": "62.95"
+            }
+        ],
+        "category": "Prosecco & Champagne",
+        "main_category": "Prosecco & Champagne"
+    },
+    {
+        "name": "Delhi 6 Crush",
+        "price": "6.95",
+        "category": "Cocktails",
+        "main_category": "Cocktails"
+    },
+    {
+        "name": "Moscow Mule",
+        "price": "6.95",
+        "category": "Cocktails",
+        "main_category": "Cocktails"
+    },
+    {
+        "name": "Blue Mumbai",
+        "price": "6.95",
+        "category": "Cocktails",
+        "main_category": "Cocktails"
+    },
+    {
+        "name": "Delhi Sunrise",
+        "price": "6.95",
+        "category": "Cocktails",
+        "main_category": "Cocktails"
+    },
+    {
+        "name": "Bombay Mango",
+        "price": "6.95",
+        "category": "Cocktails",
+        "main_category": "Cocktails"
+    },
+    {
+        "name": "Calcutta Mojito",
+        "price": "4.95",
+        "category": "Mocktails",
+        "main_category": "Mocktails"
+    },
+    {
+        "name": "Delhi Summer",
+        "price": "4.95",
+        "category": "Mocktails",
+        "main_category": "Mocktails"
+    },
+    {
+        "name": "Blue Mango",
+        "price": "4.95",
+        "category": "Mocktails",
+        "main_category": "Mocktails"
+    },
+    {
+        "name": "Goa Sunset",
+        "price": "4.95",
+        "category": "Mocktails",
+        "main_category": "Mocktails"
+    },
+    {
+        "name": "Cobra Pint",
+        "price": "5.25",
+        "category": "Draught",
+        "main_category": "Draught"
+    },
+    {
+        "name": "Cobra Half",
+        "price": "3.30",
+        "category": "Draught",
+        "main_category": "Draught"
+    },
+    {
+        "name": "San Miguel",
+        "price": "3.65",
+        "category": "Bottle Drinks",
+        "main_category": "Bottle Drinks"
+    },
+    {
+        "name": "Kingfisher",
+        "price": "3.65",
+        "category": "Bottle Drinks",
+        "main_category": "Bottle Drinks"
+    },
+    {
+        "name": "Desperados",
+        "price": "3.95",
+        "category": "Bottle Drinks",
+        "main_category": "Bottle Drinks"
+    },
+    {
+        "name": "Magners",
+        "price": "4.25",
+        "category": "Bottle Drinks",
+        "main_category": "Bottle Drinks"
+    },
+    {
+        "name": "Kopparberg (Mixed fruit)",
+        "price": "4.25",
+        "category": "Bottle Drinks",
+        "main_category": "Bottle Drinks"
+    },
+    {
+        "name": "Cobra",
+        "price": "3.65",
+        "category": "Bottle Drinks",
+        "main_category": "Bottle Drinks"
+    },
+    {
+        "name": "Non-Alcoholic Cobra",
+        "price": "3.50",
+        "category": "Bottle Drinks",
+        "main_category": "Bottle Drinks"
+    },
+    {
+        "name": "Smirnoff",
+        "price": "3.50",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Gordon's Gin",
+        "price": "3.50",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Gordon's Pink Gin",
+        "price": "3.50",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Bombay Sapphire",
+        "price": "3.95",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Whitley Neill",
+        "price": "4.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Bacardi",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Navy Rum",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Old J Spiced Rum",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Jack Daniels",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Southern Comfort",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Archers",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Tia Maria",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Sambuca",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Tequila",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Cointreau",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Disaronno",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Martini",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Pernod",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Baileys",
+        "price": "3.90",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Courvoisier",
+        "price": "4.10",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Remy Martin",
+        "price": "4.10",
+        "category": "Spirits and Liqueurs",
+        "main_category": "Spirits and Liqueurs"
+    },
+    {
+        "name": "Pepsi",
+        "prices": [
+            {
+                "type": "small",
+                "price": "2.75"
+            },
+            {
+                "type": "large",
+                "price": "4.10"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Diet Pepsi",
+        "prices": [
+            {
+                "type": "small",
+                "price": "2.75"
+            },
+            {
+                "type": "large",
+                "price": "4.10"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Lemonade",
+        "prices": [
+            {
+                "type": "small",
+                "price": "2.75"
+            },
+            {
+                "type": "large",
+                "price": "4.10"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Orange Juice",
+        "prices": [
+            {
+                "type": "small",
+                "price": "2.75"
+            },
+            {
+                "type": "large",
+                "price": "4.10"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Pineapple Juice",
+        "prices": [
+            {
+                "type": "small",
+                "price": "2.95"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Apple Juice",
+        "prices": [
+            {
+                "type": "small",
+                "price": "2.95"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "J20",
+        "prices": [
+            {
+                "type": "small",
+                "price": "2.95"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Water - Still / Sparkling",
+        "prices": [
+            {
+                "type": "small",
+                "price": "2.95"
+            },
+            {
+                "type": "large",
+                "price": "4.45"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Soda Water",
+        "prices": [
+            {
+                "type": "small",
+                "price": "2.95"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Dash of Cordial",
+        "price": "0.30",
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Cordial Drinks",
+        "prices": [
+            {
+                "type": "small",
+                "price": "1.00"
+            },
+            {
+                "type": "large",
+                "price": "1.90"
+            }
+        ],
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Bottle Coke",
+        "price": "2.95",
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Bottle Diet Coke",
+        "price": "2.95",
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Bottle Lemonade",
+        "price": "2.95",
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    },
+    {
+        "name": "Slimline Tonic",
+        "price": "2.20",
+        "category": "Soft Drinks",
+        "main_category": "Soft Drinks"
+    }
+]
